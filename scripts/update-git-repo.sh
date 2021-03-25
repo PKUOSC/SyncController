ad37bb82ea9818709302094574b5c00cb72646bf
JeremyGuo <jeremyguo@pku.edu.cn>
2021-03-25 13:34:54 +0800

    Fix some issues.

--- scripts/update-git-repo.sh
+++ scripts/update-git-repo.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 cd /data/repos/git-repos/
-if [ ! -x "$1" ]; then
-	mkdir $1
-fi
 
 if [ ! -x "$1" ]; then
+	echo "git clone --mirror $2 $1"
 	returnStatus=$(git clone --mirror $2 $1)
 else
 	cd $1
 	returnStatus=$(git remote update)
 fi
-if [[ returnStatus == 1 ]]; then
-	echo "Failed to update."
-else
+if $returnStatus; then
 	chmod 755 -R $1
-	echo "Successful."
+	echo "Success"
+	exit 0
+else
+	echo "Fail"
+	exit 1
 fi
